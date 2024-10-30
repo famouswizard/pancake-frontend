fc00a9120dbce69b4b502049b7545342bcb200ac
chefmomota <momota@pancakeswap.com>
2024-10-30 13:46:11 +0800

    feat: Change url

--- packages/position-managers/src/constants/endpoints.ts
+++ packages/position-managers/src/constants/endpoints.ts
@@ -1,3 +1,4 @@
 export const POSITION_MANAGER_API = 'https://vault.pancakeswap.com/api/statistics'
 // export const POSITION_MANAGER_API_V2 = 'https://configs.pancakeswap.com/api/data/cached/position-manager'
-export const POSITION_MANAGER_API_V2 = 'https://cms-api-prod-tfh5p.ondigitalocean.app/api/data/cached/position-manager'
+export const POSITION_MANAGER_API_V2 =
+  'https://cms-public-api-7ys4p.ondigitalocean.app/api/data/cached/position-manager'
